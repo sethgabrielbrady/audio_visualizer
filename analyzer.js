746175de9215f77ff3bab30e1bd6f48ee1b12d0e
Seth Brady <sethgabrielbrady@gmail.com>
2018-04-08 20:07:45 -0400

    updated for dynamic columns

--- analyzer.js
+++ analyzer.js
@@ -1,18 +1,28 @@
-var playing = false;
-var audio = document.getElementById('myAudio');
+let playing = false;
+let audio = document.getElementById('myAudio');
 let parent = document.getElementById('bar_test');
+let test_node = document.getElementById('test_node');
+let node;
+let nodeCount = 256;
+
+parent.setAttribute("style","grid-template-columns:repeat(" +nodeCount+ ", 1fr);");
+
+for (i=1; i <= nodeCount; i++){
+  let divNode = "node".concat(i);
+  parent.innerHTML += "<div class='node' id="+divNode+"></div>";
+}
 
 function play() {
-  var ctx = new AudioContext();
-  var audioSrc = ctx.createMediaElementSource(audio);
-  var analyser = ctx.createAnalyser();
+  let ctx = new AudioContext();
+  let audioSrc = ctx.createMediaElementSource(audio);
+  let analyser = ctx.createAnalyser();
   // we have to connect the MediaElementSource with the analyser
   audioSrc.connect(analyser);
   audioSrc.connect(ctx.destination);
   // we could configure the analyser: e.g. analyser.fftSize (for further infos read the spec)
 
   // frequencyBinCount tells you how many values you'll receive from the analyser
-  var frequencyData = new Uint8Array(analyser.frequencyBinCount);
+  let frequencyData = new Uint8Array(analyser.frequencyBinCount);
 
   function renderFrame() {
      requestAnimationFrame(renderFrame);
@@ -20,8 +30,8 @@ function play() {
      analyser.getByteFrequencyData(frequencyData);
      // render frame based on values in frequencyData
 
-    for (i=1; i <= 12; i++){
-      let node = document.getElementById("node".concat(i));
+    for (i=1; i <= nodeCount; i++){
+      node = document.getElementById("node".concat(i));
       node.setAttribute("style","height:" + frequencyData[i] + "px;");
     }
   }
